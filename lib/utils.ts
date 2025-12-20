cf10ceb0229adb7f1f5fef8964231cfb4b027486
William Chung <williamchung@Williams-MacBook-Pro.local>
2025-12-19 17:43:00 -0800

    implement rest of sign up input fields

--- lib/utils.ts
+++ lib/utils.ts
@@ -195,7 +195,16 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-export const authFormSchema = z.object({
+export const authFormSchema = (type: string) => z.object({
+  // sign up
+  fistName: type === 'sign-in' ? z.string().optional() : z.string().min(1),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(1),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
+  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+  dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(8).max(10),
+  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(4).max(4),
+  // both
   email: z.string().email(),
   password: z.string().min(8),
 });
