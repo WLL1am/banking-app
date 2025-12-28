afb09657a3a5b25abf7e2e4495c67547699f7a0b
William Chung <williamchung@Williams-MacBook-Pro.local>
2025-12-28 11:50:06 -0800

    implement recent transactions table

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,8 +4,10 @@ import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 import { getLoggedInUser } from '../../../lib/actions/user.actions'
 import { getAccount, getAccounts } from '../../../lib/actions/bank.actions'
+import RecentTransactions from '@/components/RecentTransactions'
 
 const Home = async ({ searchParams: { id, page }}: SearchParamProps) => {
+    const currentPage = Number(page as string) || 1;
     const loggedIn = await getLoggedInUser();
     const accounts = await getAccounts({ userId: loggedIn.$id })
 
@@ -39,7 +41,12 @@ const Home = async ({ searchParams: { id, page }}: SearchParamProps) => {
                     />
                 </header>
 
-                RECENT TRANSACTIONS
+                <RecentTransactions 
+                    accounts={accountsData}
+                    transactions={account?.transactions}
+                    appwriteItemId={appwriteItemId}
+                    page={currentPage}
+                />
             </div>
 
             <RightSidebar 
